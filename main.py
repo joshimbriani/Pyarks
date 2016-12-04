ce9055fd03858d790e6b6b15cefb4492070da220
Josh Imbriani <joshimbriani@gmail.com>
2016-12-04 12:22:09 -0600

    Got initial requests and wait times working

--- main.py
+++ main.py
@@ -8,10 +8,23 @@ import json
 
 requests.packages.urllib3.disable_warnings()
 
+SHARED_HEADERS = {
+    'Accept'                          : 'application/json',
+    'Accept-Language'                 : 'en-US',
+    'X-UNIWebService-AppVersion'      : '1.2.1',
+    'X-UNIWebService-Platform'        : 'Android',
+    'X-UNIWebService-PlatformVersion' : '4.4.2',
+    'X-UNIWebService-Device'          : 'samsung SM-N9005',
+    'X-UNIWebService-ServiceVersion'  : '1',
+    'User-Agent'                      : 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; SM-N9005 Build/KOT49H)',
+    'Connection'                      : 'keep-alive',
+    'Accept-Encoding'                 : 'gzip'
+  }
+
 androidKey = "AndroidMobileApp"
 androidSecret = "AndroidMobileAppSecretKey182014"
 
-date = datetime.utcnow().strftime("%A, %d %B %Y %I:%M:%S") + " GMT"
+date = datetime.utcnow().strftime("%a, %d %b %Y %H:%M:%S GMT")
 combinedKey = androidKey+ "\n" + date + "\n"
 sig = b64encode(hmac.new(androidSecret, combinedKey, sha256).digest()).strip()
 sig = re.sub("/\=$/", "\u003d", sig)
@@ -23,15 +36,20 @@ parameters = {
 
 headers = {
 		"Date": date,
-		"Content-Type": "application/json; charset=UTF-8",
-		"Accept": "application/json",
-		"Accept-Language": "en-us",
-		"User-Agent": "Mozilla/5.0 (Linux; U; Android 4.3; en-GB; C6502 Build/10.4.1.B.0.101) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30"
+		"Content-Type": "application/json; charset=UTF-8"
 		}
 
-print sig
+headers.update(SHARED_HEADERS)
 
 r = requests.post('https://services.universalorlando.com/api', headers=headers, data=json.dumps(parameters, ensure_ascii=False), verify=False)
 
+headers = {
+    'X-UNIWebService-ApiKey' : 'AndroidMobileApp',
+    'X-UNIWebService-Token' : r.json()['Token'] 
+}
 
-#print r
+headers.update(SHARED_HEADERS)
+r = requests.get('https://services.universalorlando.com/api/pointsofinterest/rides?pageSize=all', headers=headers)
+results = r.json()['Results']
+for item in results:
+	print item['MblDisplayName'], item['WaitTime']
