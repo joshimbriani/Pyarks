44f143d990a59514c4a30fdc3d544af16d5ec56a
Josh Imbriani <joshimbriani@gmail.com>
2018-10-01 20:01:34 -0500

    Updating isQueueable

--- pyarks/ride.py
+++ pyarks/ride.py
@@ -5,7 +5,7 @@ class Ride(object):
         self.waitTime = waitTime
         self.closed = (waitTime < 0 and waitTime >= -50)
         self.isWalkthrough = (waitTime == 0)
-        self.isQueueable = waitTime > -50
+        self.isQueueable = waitTime > -50 and waitTime != 0
         self.description = description
 
     def __str__(self):
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pyarks',
-      version='0.1.1',
+      version='0.1.3',
       description='A package to get wait times from amusement parks',
       url='http://github.com/joshimbriani/Pyarks',
       download_url='https://github.com/joshimbriani/Pyarks/archive/v0.1.1.tar.gz',
